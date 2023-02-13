2bc3ee64fbca2993a452362244089e9c02e53078
Elya Fileva <filevaelya@mail.ru>
2023-02-14 00:09:34 +0300

    Задача 1

---
+++ HW1-1/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите первое число: ");
+
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2)
+{
+  Console.WriteLine("max number: " + number1 + " min number: " + number2);
+}
+if (number1 < number2)
+{
+  Console.WriteLine("max number: " + number2 + " min number: " + number1);
+}
+else
+{
+  Console.WriteLine("number1=number2: " + number1);
+}
