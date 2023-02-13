a2258f78393206cbdf8a5e0e5e0798062a3869b7
Elya Fileva <filevaelya@mail.ru>
2023-02-14 00:14:24 +0300

    Задача 3

---
+++ HW1-3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+bool evenNumber = true;
+if (number % 2 == 0)
+{
+  Console.WriteLine("Число четное: " + evenNumber);
+}
+else
+{
+  Console.WriteLine("Число четное: " + !evenNumber);
+}
